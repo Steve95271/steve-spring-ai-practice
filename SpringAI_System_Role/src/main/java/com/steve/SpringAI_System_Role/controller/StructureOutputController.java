5fccd6b65697b51c8a815419d3c620a176cef146
Steve95271 <124816688+Steve95271@users.noreply.github.com>
2025-10-17 18:36:08 +0800

    update practice

---
+++ SpringAI_System_Role/src/main/java/com/steve/SpringAI_System_Role/controller/StructureOutputController.java
@@ -0,0 +1,28 @@
+package com.steve.SpringAI_System_Role.controller;
+
+import com.steve.SpringAI_System_Role.pojo.CountryCities;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class StructureOutputController {
+    private final ChatClient chatClient;
+
+    public StructureOutputController(@Qualifier("openaiChatClient") ChatClient chatClientBuilder) {
+        this.chatClient = chatClientBuilder;
+    }
+
+    @GetMapping("/chat-bean")
+    public CountryCities chatBean(String message) {
+        return chatClient
+                .prompt()
+                .user(message)
+                .call()
+                .entity(CountryCities.class);
+    }
+
+}
---
+++ SpringAI_System_Role/src/main/java/com/steve/SpringAI_System_Role/pojo/CountryCities.java
@@ -0,0 +1,6 @@
+package com.steve.SpringAI_System_Role.pojo;
+
+import java.util.List;
+
+public record CountryCities(String country, List<String> city) {
+}
