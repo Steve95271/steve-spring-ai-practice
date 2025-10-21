7f4526a246590d04291bf9f859185597a9dc2515
Steve95271 <124816688+Steve95271@users.noreply.github.com>
2025-10-21 16:46:02 +0800

    chat memory

--- SpringAI_Practice/src/main/java/com/steve/SpringAI_Practice/config/ChatClientConfig.java
+++ SpringAI_Practice/src/main/java/com/steve/SpringAI_Practice/config/ChatClientConfig.java
@@ -2,7 +2,10 @@ package com.steve.SpringAI_Practice.config;
 
 import com.steve.SpringAI_Practice.advisors.TokenUsageAuditAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiChatOptions;
@@ -29,6 +32,18 @@ public class ChatClientConfig {
         return chatClientBuilder
                 .defaultOptions(openAiChatOptions)
                 .defaultAdvisors(List.of(new SimpleLoggerAdvisor(), new TokenUsageAuditAdvisor()))
+                .build();
+    }
+
+    @Bean
+    public ChatClient chatMemoryChatClient(OpenAiChatModel openAiChatModel, ChatMemory chatMemory) {
+        Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
+        Advisor loggerAdvisor = new SimpleLoggerAdvisor();
+        OpenAiChatOptions openAiChatOptions = OpenAiChatOptions.builder().model("gpt-4o-mini").temperature(0.7).build();
+        ChatClient.Builder chatClientBuilder = ChatClient.builder(openAiChatModel);
+        return chatClientBuilder
+                .defaultOptions(openAiChatOptions)
+                .defaultAdvisors(List.of(loggerAdvisor, memoryAdvisor))
                 .build();
     }
 
